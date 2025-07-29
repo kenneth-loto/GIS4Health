c6cfee18bbf020ebba61bce48df4f80184bf6f02
Loto-Kenneth <116004412+Loto-Kenneth@users.noreply.github.com>
2025-07-29 23:36:51 +0800

    Updated suffix api code and tested the newly created utils for api and it works.

--- resources/js/api/suffix.ts
+++ resources/js/api/suffix.ts
@@ -1,32 +1,5 @@
 import type { Suffix } from '@/types';
-import axios from 'axios';
+import { DataParams, fetchOptionList, fetchTableData } from '@/utils/api-utils';
 
-export interface SuffixDataParams {
-    page?: number;
-    per_page?: number;
-    search?: string;
-}
-
-export interface SuffixDataResponse {
-    data: Suffix[];
-    last_page: number;
-    current_page: number;
-    total: number;
-}
-
-// For simple list fetch (no pagination, no search)
-export async function fetchSuffixesOption(): Promise<Suffix[]> {
-    const res = await axios.get('/api/suffixes/list');
-    return res.data.data;
-}
-
-// For dynamic list fetch (with pagination, search)
-export async function fetchSuffixesTableData(params: SuffixDataParams = { page: 1, per_page: 5 }): Promise<SuffixDataResponse> {
-    const res = await axios.get('/api/suffixes', { params });
-    return {
-        data: res.data.data,
-        current_page: res.data.meta.current_page,
-        last_page: res.data.meta.last_page,
-        total: res.data.meta.total,
-    };
-}
+export const fetchSuffixesOption = () => fetchOptionList<Suffix>('suffixes');
+export const fetchSuffixesTableData = (params?: DataParams) => fetchTableData<Suffix>('suffixes', params);
