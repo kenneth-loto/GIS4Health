a13c849b47818740367255ff8621957346cc7577
Loto-Kenneth <116004412+Loto-Kenneth@users.noreply.github.com>
2025-07-31 00:46:51 +0800

    Additional file for route.

--- routes/suffix.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\SuffixController;
-use Illuminate\Support\Facades\Route;
-
-Route::resource('suffixes', SuffixController::class)->only(['index', 'store', 'update', 'destroy'])->middleware(middleware: ['auth', 'verified']);
