f30894b2d0c46ed3ef2f7e6544a1524ab092a2c6
Loto-Kenneth <116004412+Loto-Kenneth@users.noreply.github.com>
2025-07-30 15:55:07 +0800

    Updated hook of suffix, use endpoint instead of the actual api since its already redundant.

--- resources/js/hooks/useSeverity.ts
+++ resources/js/hooks/useSeverity.ts
@@ -3,9 +3,9 @@ import { useOptionList, usePaginatedTableData } from '@/hooks/useHooks';
 import type { Severity } from '@/types';
 
 export function useSeveritiesOptionList() {
-    return useOptionList<Severity>('/api/severities/list', fetchSeveritiesOptionList);
+    return useOptionList<Severity>('severities', fetchSeveritiesOptionList);
 }
 
 export function useSeveritiesTableData(search = '', page = 1, per_page = 5) {
-    return usePaginatedTableData<Severity>('/api/severities', fetchSeveritiesTableData, search, page, per_page);
+    return usePaginatedTableData<Severity>('severities', fetchSeveritiesTableData, search, page, per_page);
 }
