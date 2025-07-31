629be51cc9fd9a164f6b96d483da84dcb651e50e
Loto-Kenneth <116004412+Loto-Kenneth@users.noreply.github.com>
2025-07-31 16:25:24 +0800

    Addtional naming issue, fixed

--- resources/js/hooks/useMunicipality.ts
+++ resources/js/hooks/useMunicipality.ts
@@ -1,9 +1,9 @@
-import { fetchMunicipalitiesOption, fetchMunicipalitiesTableData } from '@/api/municipality';
+import { fetchMunicipalitiesOptionList, fetchMunicipalitiesTableData } from '@/api/municipality';
 import { useOptionList, useTableData } from '@/hooks/useHooks';
 import type { Suffix } from '@/types';
 
 export function useMunicipalitiesOptionList() {
-    return useOptionList<Suffix>('municipalities', fetchMunicipalitiesOption);
+    return useOptionList<Suffix>('municipalities', fetchMunicipalitiesOptionList);
 }
 
 export function useMunicipalitiesTableData(search = '', page = 1, per_page = 5) {
