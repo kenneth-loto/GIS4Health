baf7c0aa5f34c3bdf1799d703bec7dbb43d30466
Loto-Kenneth <116004412+Loto-Kenneth@users.noreply.github.com>
2025-08-01 22:32:00 +0800

    Created a type for form dialog props.

---
+++ resources/js/types/dialog-props.ts
@@ -0,0 +1,8 @@
+export interface FormDialogProps<T> {
+    open: boolean;
+    onOpenChange: (open: boolean) => void;
+    initialValue: T | null;
+    isEditing: boolean;
+    isSubmitting: boolean;
+    setSubmitting: (val: boolean) => void;
+}
